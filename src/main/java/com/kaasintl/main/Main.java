787893d899ca9b8e506de52c749a61a0f1545b14
David Visscher <Davidjulianvisscher@hotmail.com>
2015-04-12 23:47:49 +0200

    Initial Commit with config

---
+++ src/main/java/com/kaasintl/main/Main.java
@@ -0,0 +1,27 @@
+package main.java.com.kaasintl.main;
+
+/**
+ * Created by David on 12-4-2015.
+ */
+public class Main
+{
+	public String host;
+	public String port;
+
+	public Main()
+	{
+		this.host = "localhost";
+		this.port = "7789";
+	}
+
+	public Main(String host, String port)
+	{
+		this.host = host;
+		this.port = port;
+	}
+
+	public static void main(String args[])
+	{
+
+	}
+}
